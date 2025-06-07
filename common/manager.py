0a1b2952693e89af4557fb1e6480bf1fc65eb3da
sudocodedev <shiva.r.160899@gmail.com>
2025-06-07 20:24:05 +0530

    base model and manager setup

---
+++ common/manager.py
@@ -0,0 +1,30 @@
+from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist, ValidationError
+from django.db.models import QuerySet
+from django.utils import timezone
+
+
+class BaseQuerySetManager(QuerySet):
+    """"""
+
+    def get_or_none(self, *args, **kwargs):
+        """"""
+        try:
+            return super().get(*args, **kwargs)
+        except (
+            AttributeError,
+            ValueError,
+            ValidationError,
+            MultipleObjectsReturned,
+            ObjectDoesNotExist
+        ):
+            return None
+
+
+    def delete(self):
+        """"""
+        return super().update(is_active=False, is_deleted=True, deleted=timezone.now())
+
+
+    def hard_delete(self):
+        """"""
+        return super().delete()
